4d2debe76a5da8fba0341dcd363135d32a5c58c2
Yintong <zhouyintong96@gmail.com>
2019-03-04 11:33:47 +0100

    fix

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,24 @@
 import 'package:angular/angular.dart';
-
+import 'src/car/car_component.dart';
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
 
 @Component(
   selector: 'my-app',
   template: '''
+    <h1>{{title}}</h1>
+    <my-car></my-car>
+    <my-injectors></my-injectors>
+    <h2>User</h2>
+    <p id="user">
+      {{userInfo}}
+      <button (click)="nextUser()">Next User</button>
+    </p>
+    <my-heroes id="authorized" *ngIf="isAuthorized"></my-heroes>
+    <my-heroes id="unauthorized" *ngIf="!isAuthorized"></my-heroes>
+    <my-providers></my-providers>
   ''',
-  directives: [coreDirectives]
+  directives: [coreDirectives, CarComponent]
 )
 class AppComponent {
   
