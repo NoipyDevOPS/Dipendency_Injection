f1817b2c0999032e39e2665f0cc228906bc396ee
Yintong <zhouyintong96@gmail.com>
2019-03-07 16:59:40 +0100

    fix

--- lib/src/car/car_factory.dart
+++ lib/src/car/car_factory.dart
@@ -1,9 +1,9 @@
 import 'car.dart';
 
 class CarFactory {
-  Car createCar() => Car(createEngine(), createTires()) ..description = 'Factory';
+  Car createCar() => Car(createEngine(), createTires()) 
+  ..description = 'Factory';
     
   Engine createEngine() => Engine();
-    
   Tires createTires() => Tires();
 }
